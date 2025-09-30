8b713ce334158c3c4a805fd15606bfdf7b960f47
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-09-30 17:16:00 +0000

    progrso de ej03

---
+++ ej03.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ej03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edad = sc.nextlnt();
+        System.out.println("Tienes (edad) años");
+    }
+}
