4ecd187cd1f476240f9abb5ac0134731c15095f4
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-01 13:32:12 +0000

    subida ej03.java

--- ej03.java
+++ ej03.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 public class ej03 {
     public static void main(String[] args) {
+        System.out.println("¿Que edad tienes?");
         Scanner sc = new Scanner(System.in);
-        int edad = sc.nextlnt();
-        System.out.println("Tienes (edad) años");
+        int edad = sc.nextInt();
+        System.out.println("Tienes " + edad + " años");
     }
 }
