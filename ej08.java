7abb164a0d02629c71407dd17ebc1c5ea464011e
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-06 17:52:16 +0000

    Terminado ej08

---
+++ ej08.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ej08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroFavorito = 24;
+        double estatura = 1.76;
+        char estadoDelCampo = 'B'; 
+        boolean deporte = true;
+        System.out.println("Tu numero favorito es: " + numeroFavorito );
+        System.out.println("Mides: " + estatura );
+        System.out.println("El estado del campo es: " + estadoDelCampo );
+        System.out.println("Hacer deporte es bueno: " + deporte );
+}
+}
