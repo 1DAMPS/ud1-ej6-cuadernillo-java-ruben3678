5b6f3b60692ba66ac086971cdcdad5efec7cfb68
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-06 17:36:57 +0000

    Terminado ej06

---
+++ ej06.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ej06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int edadDelUsuario = 18;
+        double precioTotal = 10.65;
+        char inicialDelUsuario = 'R'; 
+        System.out.println("Tu edad es " + edadDelUsuario+ " años");
+        System.out.println("El precio total es de " + precioTotal+ "€");
+        System.out.println("Tu inicial es " + inicialDelUsuario );
+}
+}
