2d0f12a0c3ca87af4307c76d098a9bcc86e77887
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-06 18:03:31 +0000

    Terminado ej09

---
+++ ej09.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ej08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroFavorito = ;
+        System.out.println("Tu numero favorito es: " + numeroFavorito );
+        // No deja inicializarlo al no tener ningun valor esa varialble 
+}
+}
