eccedb64437384ee1c599d1ce1eae72c3604b9c5
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-07 17:07:30 +0000

    subida ej12

---
+++ ej12.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ej12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float pi = 3.14;
+        System.out.println("el numero pi es " + pi );
+        // Sale un error al guardar, error de incompatibilidad y pide que uses la variable double en vez de la float
+}
+}
