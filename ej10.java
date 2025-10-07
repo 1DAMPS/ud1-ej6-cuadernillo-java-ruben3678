845debbf499fcedc3355a5a2556cf29ba442cfae
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-07 17:00:09 +0000

    subida ej11

--- ej10.java
+++ ej10.java
@@ -3,6 +3,8 @@ public class ej10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         byte edad = 18;
+        short precio = 3000;
         System.out.println("Tu tienes " + edad+ " años" );
+        System.out.println("te cuesta " + precio+ " €" );
 }
 }
---
+++ ej11.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ej11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long poblacionMundial = 800000000;
+        System.out.println("La poblacion mundial es de " + poblacionMundial+ " de habitantes en el mundo" );
+}
+}
