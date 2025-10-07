a3434d930aa1aa238717f4612bd1e82697ddb5d1
ruben3678 <ruben.garrido.ramirez.al@iespoligonosur.org>
2025-10-07 16:49:27 +0000

    subida ej10

---
+++ ej10.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class ej10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        byte edad = 18;
+        System.out.println("Tu tienes " + edad+ " años" );
+}
+}
